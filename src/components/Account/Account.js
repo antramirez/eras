9009deb58b2976eb717e73ef93d9fda2527b8ccf
Anthony Ramirez <aramirez@Anthonys-Mac-mini.local>
2021-03-24 11:06:25 -0400

    add routes, add more css, make add/edit buttons functional

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,32 +1,89 @@
+import { useRef, useEffect, useState } from 'react';
 import './Account.css';
 import editPNG from './../../assets/edit.png';
 
-const Account = () => {
-    return (
-        <article id="account" className="pa1 black-80 mb6 center mw6 account-container">
-            <h1 className="f1 mb2">Account</h1>
-            <form action="account_submit" method="get" acceptCharset="utf-8">
-                <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                    <legend className="ph0 mh0 fw6 clip">Account</legend>
-                    <div className="mt3">
-                        <label className="db fw4 lh-copy f5" htmlFor="account-firstname">First Name</label>
-                        <input className="pa2 input-reset bn w-100 measure" type="text" name="account-firstname"  id="account-firstname" value="Selam" readOnly/>
-                        <span className="edit-btn-container ml2 relative"><img src={editPNG} alt="Edit button"/></span>
+const Account = ({firstName="Selam", lastName="Moges", gradYear=2024, legal, needVisa}) => {
+    // references for each input value
+    const firstNameInputRef = useRef(null);
+    const lastNameInputRef = useRef(null);
+    const gradYearInputRef = useRef(null);
+    const legalInputRef = useRef(null);
+    const needVisaInputRef = useRef(null);
+    
+    // booleans for whether input should be edited
+    const [editFirstName, setEditFirstName] = useState(false)
+    const [editLastName, setEditLastName] = useState(false)
+    const [editGradYear, setEditGradYear] = useState(false)
+    const [editLegal, setEditLegal] = useState(false)
+    const [editNeedVisa, setEditNeedVisa] = useState(false)
 
-                    </div>
-                    <div className="mt3">
-                        <label className="db fw4 lh-copy f5" htmlFor="account-lastname">Last Name</label>
-                        <input className="pa2 input-reset bn w-100 measure" type="text" name="account-lastname"  id="account-lastname" value="Moges" readOnly/>
-                        <span className="edit-btn-container ml2 relative"><img src={editPNG} alt="Edit button"/></span>
-                    </div>
-                    <div className="mt3">
-                        <label className="db fw4 lh-copy f5" htmlFor="account-grad-year">Graduation Year</label>
-                        <input className="pa2 input-reset bn w-100 measure" type="text" name="account-grad-year"  id="account-grad-year" value="2024" readOnly/>
-                        <span className="edit-btn-container ml2 relative"><img src={editPNG} alt="Edit button"/></span>
-                    </div>
-                    <div className="mt3">
-                        <label className="db f5 fw4 lh-copy" htmlFor="account-workstatus">Work Authorization Status</label>
-                        <div className="work-status pl4">
+    const [firstNameInputValue, setFirstNameInputValue] = useState(firstName);
+    const [lastNameInputValue, setLastNameInputValue] = useState(lastName);
+    const [gradYearInputValue, setGradYearInputValue] = useState(gradYear);
+
+    // Handlers for changing readonly attribute on inputs
+    const handleFirstNameEditField = () => {
+        setEditFirstName(true)
+        if (firstNameInputRef.current) {
+            firstNameInputRef.current.focus();
+        }
+    }
+    const handleLastNameEditField = () => {
+        setEditLastName(true)
+        if (lastNameInputRef.current) {
+            lastNameInputRef.current.focus();
+        }
+    }
+    const handleGradYearEditField = () => {
+        setEditGradYear(true)
+        if (gradYearInputRef.current) {
+            gradYearInputRef.current.focus();
+        }
+    }
+
+    // Handlers for input changes
+    const handleFirstNameChange = (e) => {
+        setFirstNameInputValue(e.target.value);
+    }
+    const handleLastNameChange = (e) => {
+        setLastNameInputValue(e.target.value);
+    }
+    const handleGradYearChange = (e) => {
+        setGradYearInputValue(e.target.value);
+    }
+
+    // Handler for updating account
+    const handleSaveClick = (e) => {
+        e.preventDefault();
+        // TODO: update 
+    }
+
+    return (
+        <section id="account" className="ph4 pv4 pv5-ns ph4-m ph5-l">
+            <article className="pa1 black-80 mb6 center mw6 account-container">
+                <h1 className="f1 mb2">Account</h1>
+                <form acceptCharset="utf-8">
+                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                        <legend className="ph0 mh0 fw6 clip">Account</legend>
+                        <div className="mt3">
+                            <label className="db fw4 lh-copy f5" htmlFor="account-firstname">First Name</label>
+                            <input className="pa2 input-reset bn w-100 measure" type="text" name="account-firstname"  id="account-firstname" value={firstNameInputValue} readOnly={!editFirstName} ref={firstNameInputRef} onChange={handleFirstNameChange} />
+                            <span className="edit-btn-container ml2 relative"><img src={editPNG} alt="Edit button" onClick={handleFirstNameEditField}/></span>
+
+                        </div>
+                        <div className="mt3">
+                            <label className="db fw4 lh-copy f5" htmlFor="account-lastname">Last Name</label>
+                            <input className="pa2 input-reset bn w-100 measure" type="text" name="account-lastname"  id="account-lastname" value={lastNameInputValue} readOnly={!editLastName} ref={lastNameInputRef} onChange={handleLastNameChange} />
+                            <span className="edit-btn-container ml2 relative"><img src={editPNG} alt="Edit button" onClick={handleLastNameEditField}/></span>
+                        </div>
+                        <div className="mt3">
+                            <label className="db fw4 lh-copy f5" htmlFor="account-grad-year">Graduation Year</label>
+                            <input className="pa2 input-reset bn w-100 measure" type="text" name="account-grad-year"  id="account-grad-year" value={gradYearInputValue} readOnly={!editGradYear} ref={gradYearInputRef} onChange={handleGradYearChange} />
+                            <span className="edit-btn-container ml2 relative"><img src={editPNG} alt="Edit button" onClick={handleGradYearEditField}/></span>
+                        </div>
+                        <div className="mt3">
+                            <label className="db f5 fw4 lh-copy" htmlFor="account-workstatus">Work Authorization Status</label>
+                            <div className="work-status pl4">
                                 <div className="work-status-q-1">
                                     <p className="f6" >Are you legally allowed to work in the US?</p>
                                     <div className="flex ml4">
@@ -55,12 +112,14 @@ const Account = () => {
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                            
                         </div>
-                        
-                    </div>
-                </fieldset>
-            </form>
-        </article>
+                    </fieldset>
+                    <button className=" mt3 mb2 b ph3 pv2 input-reset ba b--black grow pointer f6" type="submit" onClick={handleSaveClick}>Save</button>
+                </form>
+            </article>
+        </section>
     )
 }
 
